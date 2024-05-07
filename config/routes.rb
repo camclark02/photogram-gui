f561285f1978ff27b4e2bdaf3843e7e911ec94f2
camclark02 <164952709+camclark02@users.noreply.github.com>
2024-05-07 03:11:20 +0000

    create image done

--- config/routes.rb
+++ config/routes.rb
@@ -7,4 +7,5 @@ get("/users/:username" , { :controller => "users", :action => "show"})
 get("/photos" , { :controller => "photos", :action => "index"})
 get("/photos/:id" , { :controller => "photos", :action => "show"})
 get("/delete_photo/:id" , { :controller => "photos", :action => "destroy"})
+post("/insert_photo" , { :controller => "photos", :action => "create"})
 end
